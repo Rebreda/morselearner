68629d4fe14a43d944cca871512ef602588c0e92
g <me@qxg.ca>
2021-11-16 17:40:10 -0800

    Remove PWA

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import * as Tone from "tone";
-import './registerServiceWorker'
 import router from './router';
 
 Vue.config.productionTip = false
-Vue.prototype.$Tone = Tone;
 
 new Vue({
   vuetify,
